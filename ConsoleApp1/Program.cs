db2654f23a7e7dc4f26feffce086b307d8ea98fe
Migoc <Migoc>
2017-04-28 11:03:24 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int ran = rnd.Next(10);
+            Console.WriteLine(ran);
+        }
+            
+    }
+}
